a358c3273a1147c738c68d5e0be9e702f60f4adf
iagotd <iagotudela@gmail.com>
2020-03-12 12:00:06 +0100

    Finished service page

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -12,4 +12,8 @@ export class ServicesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public scrollToElement(viewId){
+    document.getElementById(viewId).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+
 }
